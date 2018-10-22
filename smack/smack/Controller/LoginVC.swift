b080ed9a54d2926dfb0703b33796ba6580aae2f1
Achim Munene <achim_munene@hotmail.com>
2018-10-22 11:28:57 +0200

    Login VC done

---
+++ smack/smack/Controller/LoginVC.swift
@@ -0,0 +1,24 @@
+//
+//  LoginVC.swift
+//  smack
+//
+//  Created by Achim Munene on 22/10/2018.
+//  Copyright © 2018 Achim Munene. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func closePressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+
+}
---
+++ smack/smack/Utilities/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  smack
+//
+//  Created by Achim Munene on 22/10/2018.
+//  Copyright © 2018 Achim Munene. All rights reserved.
+//
+
+import Foundation
+
+// Segues
+let TO_LOGIN  = "toLogin"
