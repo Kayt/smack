339994a972415901a3a48ddd77cb0be94728412f
Achim Munene <achim_munene@hotmail.com>
2018-10-24 22:08:02 +0200

    alomofire added and first steps

--- smack/smack/Utilities/Constants.swift
+++ smack/smack/Utilities/Constants.swift
@@ -8,7 +8,18 @@
 
 import Foundation
 
+typealias CompletionHandler = (_ Success: Bool) -> ()
+
 // Segues
 let TO_LOGIN  = "toLogin"
 let TO_CREATE_ACCOUNT = "toCreateAccount"
 let UNWIND = "unWindToChannel"
+
+//User Defaults
+let TOKEN_KEY = "token"
+let LOGGED_IN_KEY = "loggedIn"
+let USER_EMAIL = "userEmail"
+
+//URL CONSTANTS
+let BASE_URL = "https://kimochat.herokuapp.com/v1/"
+let URL_REGISTER = "\(BASE_URL)account/register"
