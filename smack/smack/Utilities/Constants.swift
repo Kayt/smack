f99141a179a0eb0040beec572b3aa74c2d379461
Achim Munene <achim_munene@hotmail.com>
2018-10-24 23:02:05 +0200

    this is what happened

--- smack/smack/Utilities/Constants.swift
+++ smack/smack/Utilities/Constants.swift
@@ -23,3 +23,9 @@ let USER_EMAIL = "userEmail"
 //URL CONSTANTS
 let BASE_URL = "https://kimochat.herokuapp.com/v1/"
 let URL_REGISTER = "\(BASE_URL)account/register"
+let URL_LOGIN = "\(BASE_URL)account/login"
+
+//Headers
+let HEADER = [
+    "Content-Type": "application/json; charset=utf-8"
+]
