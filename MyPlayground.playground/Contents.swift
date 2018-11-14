b972f8c758b59e88be36d9e030ba7dcfa684b7aa
Achim Munene <achim_munene@hotmail.com>
2018-11-14 15:32:41 +0200

    scanner

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,7 +1,15 @@
 //: Playground - noun: a place where people can play
 
 import UIKit
-import Alamofire
 
+var str: String = "Hello, playground"
 
-var str = "Hello, playground"
+var firstname = "Achim"
+var lastName = "Munene"
+
+var fullName = "\(firstname) \(lastName)"
+
+fullName.append(" III")
+
+var bookTitle = "revenge of the titans"
+bookTitle = bookTitle.capitalized
--- smack/smack/Controller/ChannelVC.swift
+++ smack/smack/Controller/ChannelVC.swift
@@ -10,15 +10,30 @@ import UIKit
 
 class ChannelVC: UIViewController {
 
+    //Outlets
+    @IBOutlet weak var userImage: CircleImage!
     @IBOutlet weak var loginBtn: UIButton!
     @IBAction func prepareForUnwind(segue: UIStoryboardSegue){}
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.revealViewController().rearViewRevealWidth = self.view.frame.size.width - 60
+        NotificationCenter.default.addObserver(self, selector: #selector(ChannelVC.userDataDidChanged(_:)), name: NOTIF_USER_DATA_DID_CHANGE, object: nil)
     }
 
     @IBAction func loginBtnPressed(_ sender: Any) {
         performSegue(withIdentifier: TO_LOGIN, sender: nil)
+    }
+    
+    @objc func userDataDidChanged(_ notif: Notification){
+        if AuthService.instance.isLoggedIn {
+            loginBtn.setTitle(UserDataService.instance.name, for: .normal)
+            userImage.image = UIImage(named: UserDataService.instance.avatarName)
+            userImage.backgroundColor = UserDataService.instance.returnUIColor(components: UserDataService.instance.avatarColor)
+        }else{
+            loginBtn.setTitle("Login", for: .normal)
+            userImage.image = UIImage(named: "menuProfileIcon")
+            userImage.backgroundColor = UIColor.clear
+        }
     }
 }
